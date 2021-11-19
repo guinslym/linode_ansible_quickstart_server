0b9c03135a7f0a0107ce6f43acb16c367826a934
Guinslym <guinslym@users.noreply.github.com>
2021-11-19 12:18:10 -0500

    Update quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -3,11 +3,11 @@ set -e
 
 patching(){
     sudo apt update -y;
-    sudo DEBIAN_FRONTEND=noninteractive apt install vim -y;
     sudo DEBIAN_FRONTEND=noninteractive apt install wget -y;
     sudo DEBIAN_FRONTEND=noninteractive apt install ansible -y;
     wget https://raw.githubusercontent.com/guinslym/linode_ansible_quickstart_server/main/quickstart.yml    
     ansible-playbook quickstart.yml
+    sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y;
 };
 
 patching;
