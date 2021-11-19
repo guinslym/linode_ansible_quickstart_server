faa316634ee2291ec91e8f02131d16c713c00424
Guinslym <guinslym@users.noreply.github.com>
2021-11-19 12:22:39 -0500

    Update quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -7,6 +7,8 @@ patching(){
     sudo DEBIAN_FRONTEND=noninteractive apt install ansible -y;
     wget https://raw.githubusercontent.com/guinslym/linode_ansible_quickstart_server/main/quickstart.yml    
     ansible-playbook quickstart.yml
+    wget https://raw.githubusercontent.com/guinslym/linode_ansible_quickstart_server/main/install_docker.sh
+    chmod 777 install_docker.sh
     sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y;
 };
 
