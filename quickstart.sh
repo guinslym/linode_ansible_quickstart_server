339745a314e00704f0fef6d23af1d2bf775d0296
Guinslym <guinslym@users.noreply.github.com>
2021-11-19 12:09:53 -0500

    Update quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -6,6 +6,18 @@ patching(){
     sudo DEBIAN_FRONTEND=noninteractive apt install vim -y;
     sudo DEBIAN_FRONTEND=noninteractive apt install tree -y;
     sudo DEBIAN_FRONTEND=noninteractive apt install ansible -y;
+    sudo DEBIAN_FRONTEND=noninteractive apt install htop -y;
+    sudo DEBIAN_FRONTEND=noninteractive apt install ffmpeg -y;
+    sudo DEBIAN_FRONTEND=noninteractive apt install make -y;
+    sudo DEBIAN_FRONTEND=noninteractive apt install gcc -y;
+    sudo DEBIAN_FRONTEND=noninteractive apt install yamllint -y;
+    sudo DEBIAN_FRONTEND=noninteractive apt install ntp -y;
+    sudo DEBIAN_FRONTEND=noninteractive apt install curl -y;
+    sudo DEBIAN_FRONTEND=noninteractive apt install build-essential -y;
+    sudo DEBIAN_FRONTEND=noninteractive apt install libssl-dev -y;
+    sudo DEBIAN_FRONTEND=noninteractive apt install libffi-dev -y;
+    sudo DEBIAN_FRONTEND=noninteractive apt install libpq-dev -y;
+    sudo DEBIAN_FRONTEND=noninteractive apt install systat -y;
 };
 
 patching;
