e6d576ee32d3cccec6fa83be99a9cafb3d703954
Guinslym <guinslym@users.noreply.github.com>
2021-11-24 12:41:21 -0500

    Update quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -11,7 +11,7 @@ patching(){
     wget https://raw.githubusercontent.com/guinslym/linode_ansible_quickstart_server/main/install_docker.sh
     chmod 777 install_docker.sh
     sudo bash install_docker.sh
-    sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y;
+    sudo apt install neofetch
     sudo rm install_docker.sh
     sudo rm quickstart.yml
     sudo rm quickstart.sh
