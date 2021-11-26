64e0eea679eb48c0dbdbca28602b9b68bc5af1fa
Guinslym <guinslym@users.noreply.github.com>
2021-11-25 21:22:27 -0500

    Update quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -11,6 +11,9 @@ patching(){
     sudo apt install neofetch
     wget https://raw.githubusercontent.com/guinslym/linode_ansible_quickstart_server/main/docker.yml
     ansible-playbook docker.yml
+    curl -L https://github.com/docker/machine/releases/download/v0.13.0/docker-machine-`uname -s`-`uname -m` -o ~/docker-machine
+    chmod +x ~/docker-machine
+    sudo cp ~/docker-machine /usr/local/bin/
     neofetch
     sudo rm quickstart.yml
     sudo rm quickstart.sh
